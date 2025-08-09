5615a52b324778c2c871376393c126f11db36634
Ouedraogo Bonaventure Cheick Hamed <ouedraogoBonaventure@outlook.com>
2025-08-09 20:51:26 +0000

    correction routes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Rediriger toutes les routes vers index.html
+  historyApiFallback: true,
+
+  build: {
+    // Pour la production, s'assurer que les routes SPA fonctionnent
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Optimisation optionnelle
+      },
+    },
+  },
 });
